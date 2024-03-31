fbf759917ac6f189a2a8cdfd43493726092f2be5
Sarjak Patel <140601134+Sarjak0308@users.noreply.github.com>
2024-03-31 20:07:05 +0530

    Update Exp29.java

--- Exp29.java
+++ Exp29.java
@@ -3,28 +3,19 @@
   printing a count from 1 to 5 with a 1-second delay between each count
 */
 
-public class Exp29 {
-    public static void main(String[] args) {
-        
-        Thread thread1 = new Thread(new MyRunnable("Thread 1"));
-        Thread thread2 = new Thread(new MyRunnable("Thread 2"));
+class CountingThread extends Thread {
+    private int start;
+    private String threadName;
 
-        thread1.start();
-        thread2.start();
-    }
-}
-
-class MyRunnable implements Runnable {
-    private final String threadName;
-
-    public MyRunnable(String threadName) {
+    public CountingThread(String threadName, int start) {
         this.threadName = threadName;
+        this.start = start;
     }
 
     @Override
     public void run() {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(threadName + ": Count " + i);
+        for (int i = start; i <= start + 4; i++) {
+            System.out.println(threadName + "\tCount :" + i);
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -32,5 +23,14 @@ class MyRunnable implements Runnable {
             }
         }
     }
+}
 
+public class Program29_MultiThreading {
+    public static void main(String[] args) {
+        CountingThread thread1 = new CountingThread("Thread A", 1);
+        CountingThread thread2 = new CountingThread("Thread B", 6);
+
+        thread1.start();
+        thread2.start();
+    }
 }
